06573b11391dd05d74680f8d109f7e22d60730a7
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-13 11:21:04 +0900

    [update] todo list 버튼 디테일 추가 (isdone에 따른 조건)

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -10,12 +10,14 @@ export default function TodoList({
 }: Props) {
   return (
     <ul>
-      {todos.map(({ id, title, description }) => (
+      {todos.map(({ id, title, description, isDone }) => (
         <li key={id}>
           <h2>{title}</h2>
           <p>{description}</p>
           <button onClick={handleDeleteTodo(id)}>삭제</button>
-          <button onClick={handleToggleTodo(id)}>완료</button>
+          <button onClick={handleToggleTodo(id)}>
+            {!isDone ? "완료" : "취소"}
+          </button>
         </li>
       ))}
     </ul>
