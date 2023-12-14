c9641556e219079fb7b2a8e3d7ca408bb97bfe42
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-14 17:44:17 +0900

    [UPDATE] 불필요 usememo 삭제..

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { useAppDispatch, useAppSelector } from "../modules/hooks";
 import {
   addTodo as addTodoSlice,
@@ -29,25 +28,13 @@ export default function useTodos() {
     dispatch(toggleTodoSlice(id));
   };
 
-  // ? 이 부분에서 이렇게 해버린다면
-  // ? 다른 컴포넌트에서 커스텀 훅을 사용할 때마다 계산을 하기 떄문에
-  // ? 굉장히 비 효율적으로 작동할 것 같은데...
-  // ? 생각 1 : useMemo를 사용해서 쓸데없는 계산을 막는다.
-  // ? -> useMemo를 사용하면 배열이 dependency가 되는데 배열은 Object.is로 비교하면 어떤 결과가 나오지?
-  // ? -> 어차피 todos가 바뀌었다라는 것은 불변성 때문에 다른 todos
-  // ? 생각 2 : 부모 컴포넌트에서 계산한 후에 props로 내려준다.
-  // ? -> 이렇게하면 계산은 한번만 하면 되기 때문에 더 효율적일 것 같다.(현재보다는)
-  const [isDoneTodos, isNotDoneTodos] = useMemo(
-    () =>
-      todos.reduce<[Todo[], Todo[]]>(
-        (acc, todo) => {
-          if (todo.isDone) acc[TodoStatus.IS_DONE].push(todo);
-          else acc[TodoStatus.IS_NOT_DONE].push(todo);
-          return acc;
-        },
-        [[], []],
-      ),
-    [todos],
+  const [isDoneTodos, isNotDoneTodos] = todos.reduce<[Todo[], Todo[]]>(
+    (acc, todo) => {
+      if (todo.isDone) acc[TodoStatus.IS_DONE].push(todo);
+      else acc[TodoStatus.IS_NOT_DONE].push(todo);
+      return acc;
+    },
+    [[], []],
   );
 
   return {
